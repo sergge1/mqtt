43fc2bdde5e5575d60bec32bda6b715a7519cf8a
sergge1 <49363121+sergge1@users.noreply.github.com>
2020-02-10 21:48:21 +0200

    Add files via upload

---
+++ audio/audio_commands.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# coding: utf-8
+
+
+# Ключ-строки
+COMMAND_KEY = "CMD"
+SUCCESFULLY_PROCESSED_COMMAND_KEY = "SUCCESSFULLY_PROCESSED_COMMAND"
+# Строки команд
+# Движение обнаружено (сенсор движения), вкл\откл монитор и подсветку
+CMD_MOTION_ON = "MOTION_ON"
+# Движение не обнаружено (сенсор движения), вкл\откл монитор и подсветку
+CMD_MOTION_OFF = "MOTION_OFF"
+# Перезагрузка устройства
+CMD_REBOOT = "REBOOT"
+# Выключение устройства
+CMD_SHUTDOWN = "SHUTDOWN"
+# Подтверждение "Выполнено"
+CMD_SUCCESS = "SUCCESS"
+# Информирование "Не выполнено"
+CMD_FAIL = "FAIL"
+# Настройки по умолчанию применены
+CMD_FACTORY_SETTINGS = "FACTORY_SETTINGS"
